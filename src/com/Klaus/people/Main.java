4041b0b16647d0c00426ae9f4245b5846ec80b9c
noncon66 <klaus.nu@cable.vol.at>
2020-11-26 20:23:22 +0100

    more prints

--- src/com/Klaus/people/Main.java
+++ src/com/Klaus/people/Main.java
@@ -2,9 +2,9 @@ package com.Klaus.people;
 
 public class Main {
     public static void main(String[] args) {
-        People p1 = new People("Klaus", "Nussbaumer", 1978, "Bregenz", 'm');
-        People p2 = new People("Sabrina", "Vonbrühl", 1974, "Feldkirch", 'f');
-        People p3 = new People("Marcella", "Merholz", 1977, "Marktheidenfeld", 'f');
+        Person p1 = new Person("Klaus", "Nussbaumer", 1978, "Bregenz", 'm');
+        Person p2 = new Person("Sabrina", "Vonbrühl", 1974, "Feldkirch", 'f');
+        Person p3 = new Person("Marcella", "Merholz", 1977, "Marktheidenfeld", 'f');
 
         printPerson(p1);
         printPerson(p2);
@@ -12,13 +12,18 @@ public class Main {
 
         p1.setLastName("Mustermann");
 
-        printPerson(p1);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
+        System.out.println(p1.getClass().getName());
+        System.out.println(p1.getClass().getSimpleName());
 
     }
 
 
 
-    public static void printPerson (People p){
+    public static void printPerson (Person p){
         System.out.printf("%-14s %-14s %-6d %-18s %s%n", p.getFirstName(), p.getLastName(), p.getBirthYear(),p.getBirthPlace(),p.getGender());
     }
 
