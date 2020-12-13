ad5fd88b1b56ada4a9bfbe0565801afbe09942a1
noncon66 <klaus.nu@cable.vol.at>
2020-12-13 21:23:14 +0100

    initial commit

---
+++ src/com/Klaus/Zoo/Veterinary.java
@@ -0,0 +1,17 @@
+package com.Klaus.Zoo;
+
+public class Veterinary {
+    private String name;
+
+    public Veterinary(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+
+}
