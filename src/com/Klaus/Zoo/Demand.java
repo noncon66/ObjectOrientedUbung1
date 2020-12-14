9ea6fd927d84d04170848495d1d4eecd92158290
noncon66 <klaus.nu@cable.vol.at>
2020-12-14 21:18:35 +0100

    initial commit

---
+++ src/com/Klaus/Zoo/Demand.java
@@ -0,0 +1,13 @@
+package com.Klaus.Zoo;
+
+public class Demand {
+    private String name;
+    private int demand;
+
+    public Demand(String name, int demand) {
+        this.name = name;
+        this.demand = demand;
+    }
+
+
+}
