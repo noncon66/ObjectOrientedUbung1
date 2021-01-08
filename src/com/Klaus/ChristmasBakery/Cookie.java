68038accfc20f12eaa29b60e9bc5f0cbb92ef4d2
noncon66 <klaus.nu@cable.vol.at>
2021-01-08 15:05:30 +0100

    added constructor and getter for name

--- src/com/Klaus/ChristmasBakery/Cookie.java
+++ src/com/Klaus/ChristmasBakery/Cookie.java
@@ -3,5 +3,11 @@ package com.Klaus.ChristmasBakery;
 public class Cookie {
     private String name;
 
+    public Cookie(String name) {
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
 }
