3875e340f11b6355e63c5bb878cba0601d90f852
noncon66 <klaus.nu@cable.vol.at>
2021-01-08 15:20:43 +0100

    initial commit (Employees and Customers)

---
+++ src/com/Klaus/ChristmasBakery/Main.java
@@ -0,0 +1,17 @@
+package com.Klaus.ChristmasBakery;
+
+public class Main {
+    public static void main(String[] args) {
+        Bakery bakery = new Bakery("TheHeaven");
+
+        bakery.employ("Hans", "Vanillekipferl", new String[]{"Vanillekipferl", "Kokosbusserl", "Rumkugeln"});
+        bakery.employ("Karl", "Rumkugeln", new String[]{"Rumkugeln", "Nussecken", "Zimtsterne"});
+        bakery.employ("Erich", "Florentiner", new String[]{"Florentiner", "Nussecken", "Spritzgebäck", "Aniskekse"});
+
+        bakery.registerCustomer("Simone", "Landstraße 3");
+        bakery.registerCustomer("Daniela", "Stadtweg 12");
+
+
+
+    }
+}
