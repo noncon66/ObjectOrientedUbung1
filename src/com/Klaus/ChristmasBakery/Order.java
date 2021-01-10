a08794faea9a22905b9377d055e52d9e3330306b
klaus <klausnu@me.com>
2021-01-10 15:26:16 +0100

    added constructor

--- src/com/Klaus/ChristmasBakery/Order.java
+++ src/com/Klaus/ChristmasBakery/Order.java
@@ -5,4 +5,9 @@ import java.util.Vector;
 public class Order {
     private Customer customer;
     private Vector<OrderItem> OrderItems;
+
+    public Order(Customer customer, Vector<OrderItem> orderItems) {
+        this.customer = customer;
+        OrderItems = orderItems;
+    }
 }
