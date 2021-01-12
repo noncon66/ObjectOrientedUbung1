4c08a404a30c8a24f84a6b5130fdd0d9e3a1f0dc
klaus <klausnu@me.com>
2021-01-12 22:05:00 +0100

    added produce and getter

--- src/com/Klaus/ChristmasBakery/Employee.java
+++ src/com/Klaus/ChristmasBakery/Employee.java
@@ -4,31 +4,42 @@ import java.util.Vector;
 
 public class Employee {
     private String name;
-    private String favouriteCookie;
+    private Cookie favouriteCookie;
     private Vector<Cookie> cookies;
     private int productionCapacityTotal;
     private int productionCapacityLeft;
 
 
-
-    public Employee(String name, String favouriteCookie, String[] cookiesList, int productionCapacityTotal) {
+    public Employee(String name, Cookie favouriteCookie, String[] cookiesList, int productionCapacityTotal) {
         this.name = name;
         this.favouriteCookie = favouriteCookie;
         cookies = new Vector<>();
         for (var cookie :
                 cookiesList) {
-            addCookie(cookie);
-            this.productionCapacityTotal = productionCapacityTotal;
-            this.productionCapacityLeft = this.productionCapacityTotal;
+            cookies.add(new Cookie(cookie));
         }
+        this.productionCapacityTotal = productionCapacityTotal;
+        this.productionCapacityLeft = this.productionCapacityTotal;
+
     }
 
-
-    public void addCookie(String cookieName){
-        cookies.add(new Cookie(cookieName));
+    public void produce(int amount){
+        productionCapacityLeft -= amount;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Cookie getFavouriteCookie() {
+        return favouriteCookie;
+    }
+
+    public Vector<Cookie> getCookies() {
+        return cookies;
+    }
+
+    public int getProductionCapacityLeft() {
+        return productionCapacityLeft;
     }
 }
